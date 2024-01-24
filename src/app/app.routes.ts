33d89bb02d517f82c7409965560679ef79d22eef
Panthi <anish.panthi41@gmail.com>
2024-01-24 16:52:33 -0600

    Completed CRUD Operations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { UserListComponent } from './user-list/user-list.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {UserEditComponent} from "./user-edit/user-edit.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {
     path: 'home',
     component: HomeComponent
@@ -11,5 +12,9 @@ export const routes: Routes = [
   {
     path: 'users',
     component: UserListComponent
+  },
+  {
+    path: 'users/:id',
+    component: UserEditComponent
   }
 ];
