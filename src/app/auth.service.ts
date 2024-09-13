98233a4c755642636c54c1b25fc71fd8e123903a
kasireddyarun <57113718+kasireddyarun@users.noreply.github.com>
2024-09-13 09:54:27 -0500

    added headers

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,28 +1,49 @@
-import {Injectable} from '@angular/core';
-import {Location} from '@angular/common';
-import {BehaviorSubject, lastValueFrom, Observable} from 'rxjs';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {map} from 'rxjs/operators';
-import {User} from './model/user';
+import { Injectable } from '@angular/core';
+import { Location } from '@angular/common';
+import { BehaviorSubject, lastValueFrom, Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+import { User } from './model/user';
 
 const headers = new HttpHeaders().set('Accept', 'application/json');
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   $authenticationState = new BehaviorSubject<boolean>(false);
 
-  constructor(private http: HttpClient, private location: Location) {
+  constructor(private http: HttpClient, private location: Location) {}
+
+  getCookie() {
+    const token = localStorage.getItem('token');
+    return token ?? '';
+  }
+
+  private setHeaders(): HttpHeaders {
+    let headersConfig: any = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Cache-Control': 'no-store no-cache max-age=0',
+      Pragma: 'no-cache',
+      'x-csrf-token': this.getCookie(),
+    };
+    return new HttpHeaders(headersConfig);
   }
 
   getUser(): Observable<User> {
-    return this.http.get<User>('/tmv/accelerator/api/user/auth/info', {headers},)
-      .pipe(map((response: User) => {
+    return this.http
+      .get('/tmv/accelerator/api/user/auth/info', {
+        headers: this.setHeaders(),
+      })
+      .pipe(
+        tap((response: any) => {
           if (response !== null) {
+            const token = response.headers.get('x-csrf-token');
+            localStorage.setItem('token', token);
             this.$authenticationState.next(true);
           }
-          return response;
+          return response.body;
         })
       );
   }
@@ -33,13 +54,21 @@ export class AuthService {
   }
 
   login(): void {
-    location.href = `${location.origin}${this.location.prepareExternalUrl('/tmv/accelerator/api/oauth2/authorization/azure-ad')}`;
+    location.href = `${location.origin}${this.location.prepareExternalUrl(
+      '/tmv/accelerator/api/oauth2/authorization/azure-ad'
+    )}`;
   }
 
   logout(): void {
-    this.http.post('/tmv/accelerator/api/user/auth/logout',
-      {},
-      {withCredentials: true})
+    this.http
+      .post(
+        '/tmv/accelerator/api/user/auth/logout',
+        {},
+        {
+          headers: this.setHeaders(),
+          withCredentials: true,
+        }
+      )
       .subscribe((response: any) => {
         location.href = response.logoutUrl;
       });
